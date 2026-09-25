f06d588e84d5785b9d725e3709ab18bc6c69c45b
Tazwar Sikder <tazwar_rocks@hotmail.com>
2026-09-24 23:43:53 -0400

    Add transactions router to main application

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
 
-from app.routes import accounts, transfers
+from app.routes import accounts, transactions, transfers
 
 app = FastAPI(title="advisor-accounts")
 app.include_router(accounts.router)
+app.include_router(transactions.router)
 app.include_router(transfers.router)
